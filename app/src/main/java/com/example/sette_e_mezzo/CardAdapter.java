241989f335c23d80ec83dd4fe561d49005c56b8f
enricolucarelliSIRCase <enrico.lucarelli4@studio.unibo.it>
2022-08-04 13:16:28 +0200

    recycler view

---
+++ app/src/main/java/com/example/sette_e_mezzo/CardAdapter.java
@@ -0,0 +1,54 @@
+package com.example.sette_e_mezzo;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class CardAdapter  extends RecyclerView.Adapter<CardAdapter.CardViewHolder> {
+
+    ArrayList<Card> dataset;
+
+    public CardAdapter(ArrayList<Card> dataset){
+        this.dataset=dataset;
+    }
+
+    // parte java per riferirsi all'xml
+    public static class CardViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView imageView;
+
+        // View v sarà la nostra Card
+        public CardViewHolder(View v){
+            super(v);
+            imageView = v.findViewById(R.id.ivCard);
+        }
+    }
+
+    @NonNull
+    @Override
+    public CardViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        //inflate(xml,parent,false)
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_big,parent,false);
+        CardViewHolder cardViewHolder = new CardViewHolder(v);
+        return cardViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CardViewHolder holder, int position) {
+        // in questo metodo specifico il comportamento
+        holder.imageView.setImageResource(dataset.get(position).getIdImage());
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataset.size();
+    }
+
+}
+
--- app/src/main/java/com/example/sette_e_mezzo/Game2PlayersActivity.java
+++ app/src/main/java/com/example/sette_e_mezzo/Game2PlayersActivity.java
@@ -1,14 +1,51 @@
 package com.example.sette_e_mezzo;
 
+import static android.widget.LinearLayout.HORIZONTAL;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+
 
 public class Game2PlayersActivity extends AppCompatActivity {
+
+    Button btnCarta;
+    Button btnStai;
+    ArrayList<Card> listOfCards;
+
+    RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+    CardAdapter  cardAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game2players);
+
+        btnCarta = findViewById(R.id.btnCarta);
+        btnStai = findViewById(R.id.btnStai);
+
+        recyclerView = findViewById(R.id.myRecyclerView);
+
+        layoutManager = new LinearLayoutManager(this, RecyclerView.HORIZONTAL,false);
+        recyclerView.setLayoutManager(layoutManager);
+
+        listOfCards = new ArrayList<Card>();
+
+        cardAdapter = new CardAdapter(listOfCards);
+        recyclerView.setAdapter(cardAdapter);
+
+        btnCarta.setOnClickListener(v -> {
+            listOfCards.add(Deck.getIstance().pull());
+            cardAdapter.notifyItemInserted(listOfCards.size()-1);
+        });
+
     }
 }
