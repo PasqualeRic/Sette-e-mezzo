e9870de71ca7110d7ca03adb5eb0ad14edecff9f
Pasquale Ricciulli <pasquale.ricciulli@studio.unibo.it>
2022-08-10 11:28:35 +0200

    aggiunto landscape

---
+++ app/src/main/java/com/example/sette_e_mezzo/Game4PlayersActivity.java
@@ -0,0 +1,14 @@
+package com.example.sette_e_mezzo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Game4PlayersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game4_players);
+    }
+}
