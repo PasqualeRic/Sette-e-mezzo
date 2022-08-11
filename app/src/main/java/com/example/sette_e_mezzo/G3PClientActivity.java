5f496fd3cf98be97ff69fcd501beb382abc181ad
enricolucarelliSIRCase <enrico.lucarelli4@studio.unibo.it>
2022-08-11 21:45:04 +0200

    carico ed esco

--- app/src/main/java/com/example/sette_e_mezzo/G3PClientActivity.java
+++ app/src/main/java/com/example/sette_e_mezzo/G3PClientActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Game3PlayersActivity extends AppCompatActivity {
+public class G3PClientActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game3_players);
+
+
     }
 }
